7f863fb26285862deeb0528d31ab3cdd55b018c5
Slava <veaceslavcalestru@gmail.com>
2023-07-25 20:02:06 +0200

    simple LLM models

---
+++ simpleLLMmodelsIntro.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from langchain.llms import OpenAI
+load_dotenv(find_dotenv(), override=True)
+
+llm = OpenAI(model_name = 'text-davinci-003', temperature=0.7, max_tokens=512)
+
+# prompt = 'explain quantum mechanics in one sentence'
+# output = llm(prompt)
+# print(output)
+# print('\n')
+# print(llm.get_num_tokens(prompt))
+
+# output = [llm.generate(['... is the capital of France', 'What is the formula for the area of a circle?'])]
+# print(output[0].generations[0].text.strip())
+# print(len(output[0].generations))
+# print('\n')
+
+output = llm.generate(['Write an original tagline for a burger restaurant'] * 3)
+print(output)
+
+# for i in output[0].generations:
+#     print(i.text.strip())
