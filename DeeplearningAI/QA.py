5b84aafb3057d63897125a24a585c99825a26911
Slava <veaceslavcalestru@gmail.com>
2023-07-28 10:14:44 +0200

    Evaluation chain

--- DeeplearningAI/QA.py
+++ DeeplearningAI/QA.py
@@ -7,3 +7,13 @@ from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import CSVLoader
 from langchain.vectorstores import DocArrayInMemorySearch
 from IPython.display import display, Markdown
+
+file = 'C:\\Users\\User\\Desktop\\Langchain\\DeeplearningAI\\data\\OutdoorClothingCatalog_1000.csv'
+loader = CSVLoader(file_path=file)
+
+from langchain.indexes import VectorstoreIndexCreator
+index = VectorstoreIndexCreator(vectorstore_cls=DocArrayInMemorySearch).from_loaders([loader])
+
+query = "Please list all your shirts with sun protection in a table in markdown and summarize each one."
+response = index.query(query)
+print(display(Markdown(response)))
